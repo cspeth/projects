0b17e22a69ac27a38a209db417ab0dc84013ee25
csp <speth.christian@googlemail.com>
2014-05-18 20:50:24 +0200

    body of readFile of CSVReader implemented.

--- fileio/src/main/java/de/csp/fileio/csv/CSVReader.java
+++ fileio/src/main/java/de/csp/fileio/csv/CSVReader.java
@@ -3,9 +3,14 @@ package de.csp.fileio.csv;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import de.csp.fileio.to.StandardFileIoTo;
+
 public class CSVReader {
 
 	private static Log LOGGER = LogFactory.getLog(CSVReader.class);
 	
-	
+	public StandardFileIoTo readFile(String fileName) {
+		StandardFileIoTo returnTo = new StandardFileIoTo();
+		return returnTo;
+	}
 }
