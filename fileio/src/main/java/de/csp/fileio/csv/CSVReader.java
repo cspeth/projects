1ac29ee6d09e079d117415d1090e1d4ee0abaa89
cspeth <speth.christian@googlemail.com>
2014-05-25 12:03:02 +0200

    CSVReaderTest and CSVReader started to implement

--- fileio/src/main/java/de/csp/fileio/csv/CSVReader.java
+++ fileio/src/main/java/de/csp/fileio/csv/CSVReader.java
@@ -1,7 +1,13 @@
 package de.csp.fileio.csv;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import com.csvreader.CsvReader;
 
 import de.csp.fileio.to.StandardFileIoTo;
 
@@ -9,8 +15,20 @@ public class CSVReader {
 
 	private static Log LOGGER = LogFactory.getLog(CSVReader.class);
 	
-	public StandardFileIoTo readFile(String fileName) {
+	public StandardFileIoTo readFile(String fileName) throws IOException {
 		StandardFileIoTo returnTo = new StandardFileIoTo();
+		CsvReader reader = new CsvReader(fileName);
+		reader.readHeaders();
+		String[] headers = reader.getHeaders();
+		returnTo.setHeader(this.getHeaders(headers));
 		return returnTo;
+	}
+	
+	private List<String> getHeaders(String[] headers) {
+		List<String> returnList = new ArrayList<>();
+		for(String header:headers) {
+			returnList.add(header);
+		}
+		return returnList;
 	}
 }
--- fileio/src/test/java/de/csp/fileio/csv/CSVReaderTest.java
+++ fileio/src/test/java/de/csp/fileio/csv/CSVReaderTest.java
@@ -3,30 +3,42 @@ package de.csp.fileio.csv;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
 import de.csp.fileio.to.StandardFileIoTo;
 
 public class CSVReaderTest {
+	
+	private static Log LOGGER = LogFactory.getLog(CSVReaderTest.class);
 
 	@Test
 	public void testReadFile() {
 		String fileName = "readertestsemicolon.csv";
 		CSVReader csvReader = new CSVReader();
-		StandardFileIoTo to = csvReader.readFile(fileName);
-		assertEquals(3, to.getHeader().size());
-		List<String> header = to.getHeader();
-		assertTrue(header.contains("header1"));
-		assertTrue(header.contains("header2"));
-		assertTrue(header.contains("header3"));
-		assertEquals(1, to.getData().size());
-		List<Object> dataSet = to.getData().get(0);
-		assertEquals(3, dataSet.size());
-		assertTrue(dataSet.contains("data1"));
-		assertTrue(dataSet.contains("data2"));
-		assertTrue(dataSet.contains("data3"));
+		StandardFileIoTo to;
+		try {
+			to = csvReader.readFile(fileName);
+			assertEquals(3, to.getHeader().size());
+			List<String> header = to.getHeader();
+			assertTrue(header.contains("header1"));
+			assertTrue(header.contains("header2"));
+			assertTrue(header.contains("header3"));
+			assertEquals(1, to.getData().size());
+			List<Object> dataSet = to.getData().get(0);
+			assertEquals(3, dataSet.size());
+			assertTrue(dataSet.contains("data1"));
+			assertTrue(dataSet.contains("data2"));
+			assertTrue(dataSet.contains("data3"));
+		} catch (IOException e) {
+			LOGGER.error(e);
+			fail(e.toString());
+		}
+		
 	}
 
 }
