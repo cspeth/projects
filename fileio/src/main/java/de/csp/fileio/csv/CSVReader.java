d6d5826524306046d0497f3b44d81f48620dfde6
cspeth <speth.christian@googlemail.com>
2014-05-29 11:47:37 +0200

    unnecessary class omitted and imports organized

--- fileio/src/main/java/de/csp/fileio/csv/CSVReader.java
+++ fileio/src/main/java/de/csp/fileio/csv/CSVReader.java
@@ -7,9 +7,6 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import com.csvreader.CsvReader;
 
 import de.csp.fileio.to.StandardCSVFileIoTo;
--- fileio/src/test/java/de/csp/fileio/csv/CSVReaderTest.java
+++ fileio/src/test/java/de/csp/fileio/csv/CSVReaderTest.java
@@ -1,8 +1,9 @@
 package de.csp.fileio.csv;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
